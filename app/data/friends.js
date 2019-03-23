11b019375a953cd9cc1308058ac67f80e8cd1a14
Chafik <gchafik@gmail.com>
2019-03-23 10:09:53 -0700

    added logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,22 @@ let friends = [
             4,
             1
           ]
+      },{
+        
+      "name":"Joe",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,
+            1,
+            3,
+            3,
+            5,
+            1,
+            5,
+            4,
+            2,
+            4
+          ]
       }
 ];
 
